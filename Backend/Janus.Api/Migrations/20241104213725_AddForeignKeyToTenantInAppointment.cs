bceba4eab66e7149a4e4f8b3afabb6dd0f70664a
Gjorgji Dimeski <dimeskigj@gmail.com>
2024-11-04 22:39:03 +0100

    Add foreign key to Tenant in Appointment model

---
+++ Backend/Janus.Api/Migrations/20241104213725_AddForeignKeyToTenantInAppointment.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Janus.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddForeignKeyToTenantInAppointment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "TenantId",
+                table: "Appointments",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_TenantId",
+                table: "Appointments",
+                column: "TenantId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointments_Tenants_TenantId",
+                table: "Appointments",
+                column: "TenantId",
+                principalTable: "Tenants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointments_Tenants_TenantId",
+                table: "Appointments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_TenantId",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                table: "Appointments");
+        }
+    }
+}
