7e74cfc4457d92ebfb7e0c821d4418b7ea1e8852
Gjorgji Dimeski <dimeskigj@gmail.com>
2024-12-21 16:51:59 +0100

    Initialize services on FE

---
+++ Web/src/app/services/local-storage.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+  constructor() { }
+
+  setItem(key: string, value: any): void {
+    const stringValue = JSON.stringify(value);
+    localStorage.setItem(key, stringValue);
+  }
+
+  getItem<T>(key: string): T | null {
+    const item = localStorage.getItem(key);
+    return item ? (JSON.parse(item) as T) : null;
+  }
+
+  removeItem(key: string): void {
+    localStorage.removeItem(key);
+  }
+}
