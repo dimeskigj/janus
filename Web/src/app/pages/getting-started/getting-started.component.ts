d3e8e16d3a7425d574fa09d6f9a1e742c4b4e313
Gjorgji Dimeski <dimeskigj@gmail.com>
2024-12-22 21:53:57 +0100

    Handle redundant getting started

--- Web/src/app/pages/getting-started/getting-started.component.ts
+++ Web/src/app/pages/getting-started/getting-started.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Tenant } from '../../domain/tenant';
 import { CreateTenantFormComponent } from "../../components/create-tenant-form/create-tenant-form.component";
 import { LocalStorageService, keys } from '../../services/local-storage.service';
@@ -7,6 +7,7 @@ import { AuthService } from '../../services/auth.service';
 import { AsyncPipe } from '@angular/common';
 import { BehaviorSubject } from 'rxjs';
 import { User } from '@angular/fire/auth';
+import { TenantService } from '../../services/tenant.service';
 
 @Component({
   selector: 't-getting-started',
@@ -15,15 +16,24 @@ import { User } from '@angular/fire/auth';
   templateUrl: './getting-started.component.html',
   styleUrl: './getting-started.component.scss'
 })
-export class GettingStartedComponent {
+export class GettingStartedComponent implements OnInit {
   user$: BehaviorSubject<User | null> = new BehaviorSubject<User | null>(null);
 
   constructor(
     private localStorageService: LocalStorageService,
+    private tenantService: TenantService,
     authService: AuthService,
     private router: Router
   ) {
     this.user$ = authService.user$;
+  }
+
+  ngOnInit(): void {
+    this.tenantService.getTenantsForUser().subscribe({
+      next: (tenants) => {
+        if (tenants?.length) this.router.navigateByUrl('upcoming');
+      }
+    })
   }
 
   onTenantCreated(tenant: Tenant): void {
