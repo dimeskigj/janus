31066bfe73541003acb66fa7d67bcf3ca0d6f6ef
Gjorgji Dimeski <dimeskigj@gmail.com>
2024-12-28 21:48:25 +0100

    Add share dialog

---
+++ Web/src/app/components/dialogs/share-business-dialog/share-business-dialog.component.ts
@@ -0,0 +1,43 @@
+import { Component, Inject, ViewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { QRCodeComponent, QRCodeModule } from 'angularx-qrcode';
+import { Tenant } from '../../../domain/tenant';
+import { SafeUrl } from '@angular/platform-browser';
+
+@Component({
+  selector: 't-share-business-dialog',
+  standalone: true,
+  imports: [MatIconModule, MatButtonModule, MatTooltipModule, MatDialogModule, QRCodeModule],
+  templateUrl: './share-business-dialog.component.html',
+  styleUrl: './share-business-dialog.component.scss'
+})
+export class ShareBusinessDialogComponent {
+  isCopied = false;
+  qrCodeDownloadLink?: string;
+
+  constructor(
+    public dialogRef: MatDialogRef<ShareBusinessDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public tenant: Tenant,
+    private clipboard: Clipboard
+  ) { }
+
+
+  get publicUrl(): string {
+    return `https://app.termince.com/t/${this.tenant?.slug}`;
+  }
+
+  copyPublicUrl(): void {
+    this.clipboard.copy(this.publicUrl);
+    this.isCopied = true;
+
+    setTimeout(() => this.isCopied = false, 2000);
+  }
+
+  onChangeQrUrl(url: SafeUrl) {
+    this.qrCodeDownloadLink = url as string;
+  }
+}
--- Web/src/app/components/selected-tenant/selected-tenant.component.ts
+++ Web/src/app/components/selected-tenant/selected-tenant.component.ts
@@ -1,32 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { Tenant } from '../../domain/tenant';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { Clipboard } from '@angular/cdk/clipboard';
+import { MatDialog } from '@angular/material/dialog';
+import { ShareBusinessDialogComponent } from '../dialogs/share-business-dialog/share-business-dialog.component';
 
 @Component({
   selector: 't-selected-tenant',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatTooltipModule],
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './selected-tenant.component.html',
   styleUrl: './selected-tenant.component.scss'
 })
 export class SelectedTenantComponent {
   @Input()
   tenant?: Tenant;
-  isCopied = false;
+  dialog = inject(MatDialog);
 
-  constructor(private clipboard: Clipboard) { }
-
-  get publicUrl(): string {
-    return `https://app.termince.com/t/${this.tenant?.slug}`;
-  }
-
-  copyPublicUrl(): void {
-    this.clipboard.copy(this.publicUrl);
-    this.isCopied = true;
-
-    setTimeout(() => this.isCopied = false, 2000);
+  openShareDialog(): void {
+    this.dialog.open(ShareBusinessDialogComponent, { data: this.tenant });
   }
 }
