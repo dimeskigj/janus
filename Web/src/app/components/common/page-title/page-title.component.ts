286922f10a8f8c33a5e74b2befc93b78d819a994
Gjorgji Dimeski <dimeskigj@gmail.com>
2024-12-31 21:26:16 +0100

    Add back button functionality

--- Web/src/app/components/common/page-title/page-title.component.ts
+++ Web/src/app/components/common/page-title/page-title.component.ts
@@ -1,11 +1,14 @@
+import { Location } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 't-page-title',
   standalone: true,
-  imports: [MatIconModule, MatProgressBarModule],
+  imports: [MatIconModule, MatProgressBarModule, MatButtonModule, MatIconModule, RouterModule],
   templateUrl: './page-title.component.html',
   styleUrl: './page-title.component.scss'
 })
@@ -13,4 +16,6 @@ export class PageTitleComponent {
   @Input() title?: string;
   @Input() icon?: string;
   @Input() isLoading = false;
+  @Input() hasBackButton = false;
+  @Input() backLink?: string;
 }
