3b66312462c3847ee47e76f8d41ee85d2776f71d
Gjorgji Dimeski <dimeskigj@gmail.com>
2025-01-22 00:05:16 +0100

    Add Number and LanguageCode to Appointment

--- Backend/Janus.Api/Migrations/20250121230326_AddLanguageAndNumberToAppointment.Designer.cs
+++ Backend/Janus.Api/Migrations/20250121230326_AddLanguageAndNumberToAppointment.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Janus.Api.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Janus.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250121230326_AddLanguageAndNumberToAppointment")]
+    partial class AddLanguageAndNumberToAppointment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Backend/Janus.Api/Models/Appointment.cs
+++ Backend/Janus.Api/Models/Appointment.cs
@@ -1,4 +1,6 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Janus.Api.Models;
 
@@ -8,6 +10,10 @@ public class Appointment
     public Guid AppointmentSlotId { get; init; }
     public Guid TenantId { get; set; }
     public Tenant? Tenant { get; set; }
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int AppointmentNumber { get; init; }
+
     public required AppointmentSlot AppointmentSlot { get; init; }
     [MaxLength(100)] public required string Email { get; init; }
     [MaxLength(100)] public required string Phone { get; init; }
@@ -15,4 +21,5 @@ public class Appointment
     [MaxLength(1000)] public required string Notes { get; init; }
     public bool IsConfirmed { get; init; }
     public bool IsCanceled { get; init; }
+    [MaxLength(5)] public string LanguageCode { get; set; } = "en";
 }
